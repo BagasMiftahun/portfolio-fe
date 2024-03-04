e53adaa128a1cb49e67fecc1bb39238a9ad32437
Bagas Miftahun Na'im <bagasmiftahunaim@gmail.com>
2024-03-04 23:26:03 +0700

    Add - create Hero and Project section in Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <nav class="flex flex-col pb-12 justify-center items-center gap-y-8">
+  <nav class="flex flex-col pb-2 justify-center items-center gap-y-8">
     <img src="./assets/logo-1.png" alt="logo">
     <div class="flex flex-row justify-center items-center rounded-3xl gap-x-1 bg-black-500 text-grey-500 p-1 group">
-      <router-link to="/" class="px-4 py-2 transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl">Home</router-link>
-      <div class="flex flex-row justify-center items-center px-4 py-2 gap-x-1 group transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl">
+      <router-link to="/" class="px-4 py-2 transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl" :class="{ 'text-black-500 bg-white-500': !$route.path || $route.path === '/' }">Home</router-link>
+      <div class="flex flex-row justify-center items-center px-4 py-2 gap-x-1 group transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl" :class="{ 'text-black-500 bg-white-500': $route.path === '/project' }">
         <span class="text-sm bg-grey-500 text-black-500 rounded-full px-1 transition-all duration-300 ease-in-out transform hover:-translate-x-1">12</span>
         <router-link to="/project">Project</router-link>
       </div>
-      <router-link to="/about" class="px-4 py-2 transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl">About</router-link>
-      <router-link to="/contact" class="px-4 py-2 transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl">Contact</router-link>
+      <router-link to="/about" class="px-4 py-2 transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl" :class="{ 'text-black-500 bg-white-500': $route.path === '/about' }">About</router-link>
+      <router-link to="/contact" class="px-4 py-2 transition-colors duration-300 ease-in-out hover:text-black-500 hover:bg-grey-500 rounded-3xl" :class="{ 'text-black-500 bg-white-500': $route.path === '/contact' }">Contact</router-link>
     </div>
   </nav>
   <router-view/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,103 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" class="items-center"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="../assets/logo.png" class="items-center"> -->
+  <div id="home">
+    <section class="layout max-w-6xl mx-auto pb-12">
+      <div class="flex flex-row justify-between">
+        <h1 class="text-7xl font-medium text-black-500 leading-none mb-10">
+          Websites&<br />Branding
+        </h1>
+      </div>
+      <div class="flex flex-row items-center justify-between">
+        <div class="flex flex-col">
+          <h4 class="text-xl font-medium">Let's talk</h4>
+          <p class="text-lg font-regular">bagasmiftahunaim@gmail.com</p>
+        </div>
+        <p class="text-lg font-regular">
+          Hello, I'm Joey, an online product designer focusing on <br />brand
+          identity, advertising, and no-code instruments.
+        </p>
+      </div>
+    </section>
+    <section class="project max-w-6xl mx-auto py-12">
+      <div
+        class="flex flex-col md:flex-row justify-between items-start md:items-center"
+      >
+        <div class="flex flex-col gap-y-7">
+          <h2
+            class="text-2xl font-medium text-black-500 leading-8 -tracking-1 mb-2"
+          >
+            Projects
+          </h2>
+          <p class="text-lg font-regular">
+            As a seasoned creator of contemporary, user-<br />
+            friendly web designs and digital solutions, I aim <br />
+            to assist you in constructing the brand of your <br />
+            fantasies.
+          </p>
+        </div>
+        <h5 class="text-lg mt-4 md:mt-0 md:ml-4">More -></h5>
+      </div>
+      <div class="grid grid-cols-1 gap-x-8 gap-y-12 mt-5 md:grid-cols-2">
+        <div class="flex flex-col gap-y-6">
+          <img
+            src="../assets/images/project-1.png"
+            alt=""
+            class="rounded-3xl max-w-none"
+          />
+          <div class="flex flex-col gap-y-1">
+            <h5 class="text-lg font-medium">Check</h5>
+            <p class="font-regular">Mobile App</p>
+          </div>
+        </div>
+        <div class="flex flex-col gap-y-6">
+          <img
+            src="../assets/images/project-1.png"
+            alt=""
+            class="rounded-3xl max-w-none"
+          />
+          <div class="flex flex-col gap-y-1">
+            <h5 class="text-lg font-medium">Check</h5>
+            <p class="font-regular">Mobile App</p>
+          </div>
+        </div>
+        <div class="flex flex-col gap-y-6">
+          <img
+            src="../assets/images/project-1.png"
+            alt=""
+            class="rounded-3xl max-w-none"
+          />
+          <div class="flex flex-col gap-y-1">
+            <h5 class="text-lg font-medium">Check</h5>
+            <p class="font-regular">Mobile App</p>
+          </div>
+        </div>
+        <div class="flex flex-col gap-y-6">
+          <img
+            src="../assets/images/project-1.png"
+            alt=""
+            class="rounded-3xl max-w-none"
+          />
+          <div class="flex flex-col gap-y-1">
+            <h5 class="text-lg font-medium">Check</h5>
+            <p class="font-regular">Mobile App</p>
+          </div>
+        </div>
+        <!-- Add more similar div elements as needed -->
+      </div>
+    </section>
   </div>
+
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+// export default {
+//   name: "HomeView",
+//   components: {
+//     HelloWorld,
+//   },
+// };
 </script>
